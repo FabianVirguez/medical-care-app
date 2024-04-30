fe49809ece5bee151f035c27316ffec265d91d8a
Fabian Virguez <fabian.virguez@infracommerce.lat>
2024-04-30 13:17:14 -0500

    chore: :wrench: update file constants settings

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 export const ENV = {
-  // SERVER_HOST: "https://strapi-services.up.railway.app",
-  // API_URL: "https://strapi-services.up.railway.app/api",
-  SERVER_HOST: "http://localhost:1337",
-  API_URL: "http://localhost:1337/api",
+  SERVER_HOST: "https://services-medical-care-production.up.railway.app",
+  API_URL: "https://services-medical-care-production.up.railway.app/api",
+  // SERVER_HOST: "http://localhost:1337",
+  // API_URL: "http://localhost:1337/api",
   ENDPOINTS: {
     AUTH: {
       REGISTER: "auth/local/register",
